f0dc9674b08e7af86755ace4fa4b06bd60c8b5d8
teirm <tribunus.vii@gmail.com>
2019-07-12 21:09:34 -0400

    Updated outline program

--- ch_5/outline2.go
+++ ch_5/outline2.go
@@ -2,6 +2,19 @@
 // x in the tree rooted at n.  Both functions are optional.
 // pre is called before the children are visited (preorder) and
 // post is called after (postorder)
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+var depth int
+
 func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
 	if pre != nil {
 		pre(n)
@@ -18,7 +31,11 @@ func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
 
 func startElement(n *html.Node) {
 	if n.Type == html.ElementNode {
-		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+		if n.FirstChild != nil {
+			fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+		} else {
+			fmt.Printf("%*s<%s\\>\n", depth*2, "", n.Data)
+		}
 		depth++
 	}
 }
@@ -26,6 +43,24 @@ func startElement(n *html.Node) {
 func endElement(n *html.Node) {
 	if n.Type == html.ElementNode {
 		depth--
-		fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+		if n.FirstChild != nil {
+			fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+		}
 	}
+}
+
+func main() {
+
+	resp, err := http.Get(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "outline2: %v\n", err)
+	}
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "outline2: %v\n", err)
+		os.Exit(1)
+	}
+	resp.Body.Close()
+	forEachNode(doc, startElement, endElement)
 }
