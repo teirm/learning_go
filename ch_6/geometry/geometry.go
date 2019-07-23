b35ecdd2ffa60f814ddee8c4f929e1d035a1fc5e
teirm <tribunus.vii@gmail.com>
2019-07-22 21:06:59 -0400

    OOP in go

---
+++ ch_6/geometry/geometry.go
@@ -0,0 +1,29 @@
+// tools for plane geometry
+package geometry
+
+import "math"
+
+type Point struct{ X, Y float64 }
+
+type Path []Point
+
+// traditional function
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+// same thing but as a method of the Point type
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+// Distance returns the distance traveled along the path
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
+}
