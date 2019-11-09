a0ffe616a85228bbbd917d54a9f2188d1a94f25d
teirm <tribunus.vii@gmail.com>
2019-11-08 19:17:36 -0500

    Notes on depth limiting url crawl

--- ch_8/crawler/crawler.go
+++ ch_8/crawler/crawler.go
@@ -13,6 +13,20 @@ import (
 // tokens is a counting semaphore used to
 // enforce a limit of 20 concurrent requests
 
+// TODO(teirm) notes on rate limiting -- can have each
+// link be in a struct with depth -- don't add to
+// worklist if depth is greater than desired depth OR
+// don't spawn a goroutine if greater than desired depth
+
+// need some function to convert []string into []CrawlerTask
+// func createCrawlerTask(url string, depth int) CrawlerTask
+// func createCrawlerTasks(url []string, depth int) []CrawlerTask
+
+type CrawlerTask struct {
+	url   string
+	depth int
+}
+
 var tokens = make(chan struct{}, 20)
 
 func crawl(url string) []string {
